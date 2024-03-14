825926b8a86ebbe5e5c88b2f16df2177778da882
Mihail Komarov <michaelkomarov03@gmail.com>
2024-03-14 16:02:14 +0200

    _1_Classes_and_Objects.exercise1: Pair implementation

---
+++ _1_Classes_and_Objects/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+    static {
+        //some hidden verification code inside this block
+    }
+    private static void output(Pair pair) {
+        System.out.printf("a = %d, b = %d\n", pair.getA(), pair.getB());
+        System.out.printf("sum = %d, max = %d\n", pair.sum(), pair.max());
+    }
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int a0 = sc.nextInt();
+            int b0 = sc.nextInt();
+            Pair pair0 = new Pair(a0, b0);
+            output(pair0);
+            Pair pair1 = new Pair();
+            int a1 = sc.nextInt();
+            int b1 = sc.nextInt();
+            pair1.setA(a1);
+            pair1.setB(b1);
+            output(pair1);
+        }
+    }
+}
---
+++ _1_Classes_and_Objects/src/Pair.java
@@ -0,0 +1,35 @@
+public class Pair {
+    private int a;
+    private int b;
+
+    public Pair() { }
+
+    public Pair(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int sum() {
+        return a + b;
+    }
+
+    public int max() {
+        return Math.max(a, b);
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
