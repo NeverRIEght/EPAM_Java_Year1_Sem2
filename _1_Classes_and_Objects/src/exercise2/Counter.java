3232b57413e0828ad4adf092df88edc2e936e7ca
Mihail Komarov <michaelkomarov03@gmail.com>
2024-03-24 13:28:47 +0200

    _1_Classes_and_Objects.exercise2: Counter implementation

---
+++ _1_Classes_and_Objects/src/exercise2/Counter.java
@@ -0,0 +1,41 @@
+package exercise2;
+
+class Counter {
+    private int state;
+
+    private final static int DOWN_BOUND = 5;
+
+    private final static int UP_BOUND = 10;
+
+    public Counter() {
+        this.state = (DOWN_BOUND + UP_BOUND) / 2;
+    }
+
+    public Counter(int state) {
+        if (state <= DOWN_BOUND) {
+            this.state = DOWN_BOUND;
+        } else {
+            this.state = Math.min(state, UP_BOUND);
+        }
+    }
+
+    public void inc() {
+        if (state >= UP_BOUND) {
+            this.state = UP_BOUND;
+        } else {
+            this.state++;
+        }
+    }
+
+    public void dec() {
+        if (state <= DOWN_BOUND) {
+            this.state = DOWN_BOUND;
+        } else {
+            this.state--;
+        }
+    }
+
+    public int getState() {
+        return this.state;
+    }
+}
