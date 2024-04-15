7677ef390e9343e6840f54a2bda7f3f781a4e83b
Mihail Komarov <michaelkomarov03@gmail.com>
2024-04-15 18:14:04 +0300

    _1_Classes_and_Objects.section2.exercise2: add Trip

--- _2_Inheritance_and_Interfaces/src/section2/exercise2/Trip.java
+++ _2_Inheritance_and_Interfaces/src/section2/exercise2/Trip.java
@@ -4,50 +4,57 @@ import java.util.Scanner;
 
 class Trip {
     //declare class and instance constants according to the Java Code Conventions
-    private final int dailyRate;
+    private static final int DAILY_RATE = 50;
     private final int numberOfDays;
     private final int fare;
 
     //declare a parameterized constructor
     public Trip(int numberOfDays, int fare) {
-        this.dailyRate = 50;
         this.numberOfDays = numberOfDays;
         this.fare = fare;
     }
 
     public final int totalCost() {
         //write the correct expression instead of 0
-        return dailyRate * numberOfDays + fare;
+        return getDailyRate() * numberOfDays + fare;
     }
 
     //add protected method and use it the totalCost()
+    protected int getDailyRate() {
+        return DAILY_RATE;
+    }
 }
 
 class HomeTrip extends Trip {
     //declare class constant
-    private final int dailyRate;
+    private static final int DAILY_RATE = 60;
 
     //declare a parameterized constructor
     public HomeTrip(int numberOfDays) {
         super(numberOfDays, 0);
-        this.dailyRate = 60;
     }
 
     //override the protected method
+    @Override
+    protected int getDailyRate() {
+        return DAILY_RATE;
+    }
 }
 
 class OneDayTrip extends Trip {
     //declare class constant
-    private final int dailyRate;
+    private static final int DAILY_RATE = 35;
     //declare a parameterized constructor
 
     public OneDayTrip(int fare) {
         super(1, fare);
-        this.dailyRate = 35;
     }
 
     //override the protected method
-
+    @Override
+    protected int getDailyRate() {
+        return DAILY_RATE;
+    }
 }
 
 class Solution {
