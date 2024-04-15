726aad77683463de8280a15288bfd524178afeb9
Mihail Komarov <michaelkomarov03@gmail.com>
2024-04-15 18:13:52 +0300

    _1_Classes_and_Objects.section1.exercise2: Karamach version

--- _2_Inheritance_and_Interfaces/src/section1/exercise2/Pair.java
+++ _2_Inheritance_and_Interfaces/src/section1/exercise2/Pair.java
@@ -12,11 +12,16 @@ class Pair {
     @Override
     public boolean equals(Object obj) {
         boolean result = false;
-        Pair objPair = (Pair) obj;
-        if (this.a == objPair.a && this.b == objPair.b) {
-            result = true;
-        } else if (this.a == objPair.b) {
-            result = true;
+
+        if(obj.getClass().equals(Pair.class) || obj.getClass().equals(ColoredPair.class)) {
+            Pair objPair = (Pair) obj;
+            if (this.a == objPair.a && this.b == objPair.b) {
+                result = true;
+            } else if (this.a == objPair.b) {
+                result = true;
+            } else if (this.b == objPair.a) {
+                result = true;
+            }
         }
 
         return result;
